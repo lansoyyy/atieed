730e5c78e3e427233e39c78e7c895233f73e5939
Lans12345 <olanalans12345@gmail.com>
2024-04-24 11:32:11 +0800

    added palletes

--- lib/utlis/colors.dart
+++ lib/utlis/colors.dart
@@ -2,8 +2,10 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-var primary = const Color(0xff1A1851);
+var primary = const Color(0xffFFC961);
 
-var secondary = const Color(0xffFCB315);
+var secondary = const Color(0xff1F2A37);
 var black = const Color(0xff151515);
-var grey = const Color(0xffB3B3B3);
+var grey = const Color(0xffEAEAEA);
+var green = const Color(0xff7ED957);
+var red = const Color(0xffFF5757);
