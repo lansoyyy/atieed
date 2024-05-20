493d0ebcfc451d53429d89a222c2f3d9ea165433
Lans12345 <olanalans12345@gmail.com>
2024-05-21 07:20:09 +0800

    added chat func

--- lib/utlis/colors.dart
+++ lib/utlis/colors.dart
@@ -6,6 +6,6 @@ var primary = const Color(0xffFFC961);
 
 var secondary = const Color(0xff1F2A37);
 var black = const Color(0xff151515);
-var grey = const Color(0xffEAEAEA);
+var grey = const Color.fromARGB(255, 134, 130, 130);
 var green = const Color(0xff7ED957);
 var red = const Color(0xffFF5757);
